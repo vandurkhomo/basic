57c1f9fb551bad0a3a29a3d2d545d334e911f739
vanduckhomo <2961779669@qq.com>
2023-10-26 00:30:32 +0800

    基本完成封装（未封装含挑战卡部分），实现简单按键流控制

--- src/move.py
+++ src/move.py
@@ -2,10 +2,32 @@ from robomaster import robot
 import time
 from pynput import keyboard
 import threading
+from threading import Event
 
 
 class MoveControl:
+    """
+    动作控制类
+    （没有封装含挑战卡的函数）
+        主要是对robot.Drone().flight的封装
+            封装了前后左右上下飞行、停止
+            封装了电机的开关
+            封装了起飞和降落
+            封装了角度旋转
+            封装了控制飞机遥控器的四个杆量（ 横滚 俯仰 油门 偏航）
+            实现了速度控制，飞行的距离控制
+            实现了键盘流控制飞机的功能
+            速度获取，角速度获取不在此类中（可能在传感器类中）
+
+    参数：
+        self.step  ：每次飞行的步长
+        self.move ： robot.Drone().flight的别名
+            （控制同一飞机时传入相同的robot.Drone().flight对象）
+        self.speed ：飞行的速度
+        self.keyboard ：KeyBoard键盘捕获类
+    """
     def __init__(self, flight, speed=10):
+        self.step = 100
         self.move = flight
         self.speed = speed
         self.keyboard = KeyBoard(self)
@@ -46,7 +68,7 @@ class MoveControl:
             direction:
                 飞行的方向，”forward” 向前飞行， “back” 向后飞行， “up” 向上飞行， “down” 向下飞行， “left” 向左飞行， “right” 向右飞行
             distance:
-                [20, 500]，飞行的距离，单位 cm
+                [20, 500]，飞行的距离，单位 cm,无则默认为self.step
             retry:
                 是否重发命令
             timeout:
@@ -54,6 +76,8 @@ class MoveControl:
         Returns:无
 
         """
+        if not distance:
+            distance = self.step
         if direction:
             action = self.move.fly(direction, distance, retry)
             if timeout:
@@ -114,7 +138,7 @@ class MoveControl:
         """
         return self.move.stop(retry)
 
-    def _speed(self, speed: float = 0):
+    def speed_(self, speed: float = 0):
         """
         无参数传入:获取当前速度
         有参数传入：修改当前速度
@@ -124,6 +148,8 @@ class MoveControl:
         :return:
             更改后的速度
         """
+        if 100 < speed or speed < 10:
+            speed = 0
         if speed:
             self.speed = speed
         if not self.move.set_speed(self.speed):
@@ -142,16 +168,56 @@ class MoveControl:
         """
         self.move.rc(a, b, c, d)
 
+    def takeoff(self, retry=True, timeout=0):
+        """
+        起飞（约40cm）
+        :param retry:
+            是否重发命令
+        :param timeout:
+            等待动作完成最大的时间（启用则有阻塞）
+
+        :return: 无
+        """
+        action = self.move.takeoff(retry)
+        if timeout:
+            action.wait_for_completed(timeout)
+
     def keyboard_listen(self):
-        self.keyboard._start()
+        """
+        键盘捕获
+            有阻塞，实现键盘流控制
+                self.keyboard是类KeyBoard的实例（具体实现见类KeyBoard）
+        结束后重置监听线程 keyboard.Listener
+        :return: 无
+        """
+        print("up,down,left,right,1,2,3,4 only! "
+              "press esc to quit~")
+        self.keyboard.listening = 1
+        self.keyboard.listener.start()
         while self.keyboard.listening:
             pass
+        self.keyboard.listener = keyboard.Listener(
+            on_press=self.keyboard.key_in,
+        )
 
-    # def move_flow(self, move_flow_list: list, ):
-    #     pass
+    def set_step(self, new_step):
+        """
+        设置新飞行步长
+        :param new_step: 设置的新飞行步长
+        :return: 无
+        """
+        if 500 >= new_step >= 20:
+            self.step = new_step
 
 
 class KeyBoard:
+    """
+    键盘捕获
+        用keyboard.Listener实现（自带多线程）
+        在MoveControl.keyboard_listen（）被调用（keyboard_listen有阻塞）
+    由于self.father直接传入了MoveControl的实例对象，因此可以直接调用传入对象的方法
+
+    """
     def __init__(self, father):
         self.listening = 0
         self.father = father
@@ -160,37 +226,64 @@ class KeyBoard:
         )
 
     def key_in(self, key):
+        """
+        keyboard.Listener 的 on_press回调函数，按键摁下触发
 
-        if key == keyboard.Key.esc:
-            print("end")
-            self.listening = 0
-            self.listener.stop()
-        elif key == keyboard.Key.up:
-            self.father.fly("forward", 20)
-        elif key == keyboard.Key.down:
-            self.father.fly("back", 20)
-        elif key == keyboard.Key.left:
-            self.father.fly("left", 20)
-        elif key == keyboard.Key.right:
-            self.father.fly("right", 20)
-        else:
-            print("up down left right only! "
-                  "press esc to quit~")
+        修改该函数可以自定义键盘流控制飞机行为
 
-    def _start(self):
-        listener_thread = threading.Thread(target=self.listener.start)
-        listener_thread.daemon = True
-        listener_thread.start()
-        self.listening = 1
+        目前行为：
+            key 1:
+                飞行步长增加（20）
+            key 2:
+                飞行步长减少（20）
+            key 3:
+                飞行速度增加（10）
+            key 4:
+                飞行步长减少（10）
+            key up:
+                向前飞
+            key down:
+                向后飞
+            key left:
+                向左飞
+            key right:
+                向右飞
+        :param key: 键盘捕获
+        :return: 无
+        """
+        try:
+            if key.char == '1':
+                self.father.set_step(self.father.step + 20)
+            if key.char == '2':
+                self.father.set_step(self.father.step - 20)
+            if key.char == '3':
+                self.father.speed_(self.father.speed + 10)
+            if key.char == '4':
+                self.father.speed_(self.father.speed - 10)
+        except AttributeError:
+            if key == keyboard.Key.esc:
+                print("end")
+                self.listening = 0
+                self.listener.args = 0
+                self.listener.stop()
+                del self.listener
+            elif key == keyboard.Key.up:
+                self.father.fly("forward", self.father.step)
+            elif key == keyboard.Key.down:
+                self.father.fly("back", self.father.step)
+            elif key == keyboard.Key.left:
+                self.father.fly("left", self.father.step)
+            elif key == keyboard.Key.right:
+                self.father.fly("right", self.father.step)
 
 
 if __name__ == '__main__':
-    # tl_drone = robot.Drone()
-    # tl_drone.initialize()
-    # tl_flight = tl_drone.flight
-    tl_flight = 1
+    tl_drone = robot.Drone()
+    tl_drone.initialize()
+    tl_flight = tl_drone.flight
     move_ = MoveControl(tl_flight)
+    move_.takeoff()
     move_.keyboard_listen()
-    # del move_.speed
-    # time.sleep(6)
-    # tl_drone.close()
+
+
+# ERROR action.py:355 Robot is already performing 500 action(s) <action, name:FlightAction, state:action_started
